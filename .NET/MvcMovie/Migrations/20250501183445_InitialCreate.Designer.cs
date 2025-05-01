cd3f8f7ec74c9f3b1e1adeabd0cb4d768730380c
MariaGalecka <galecm5715195_aehit@students.vizja.pl>
2025-05-01 20:36:31 +0200

    Cz. 8 Dodawanie nowego pola

--- .NET/MvcMovie/Migrations/20250501183445_InitialCreate.Designer.cs
+++ .NET/MvcMovie/Migrations/20250501183445_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20250501163843_InitialCreate")]
+    [Migration("20250501183445_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -30,6 +30,9 @@ namespace MvcMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
